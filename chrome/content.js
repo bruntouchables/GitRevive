2e1f670a803b6d44dd1720dde865286d04a53245
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-08-30 12:04:30 +0400

    Update content.js

--- chrome/content.js
+++ chrome/content.js
@@ -3,22 +3,20 @@
  */
 
 
-'use strict';
-
-let observer = new MutationObserver(mutations => {
+const observer = new MutationObserver(mutations => {
   mutations.forEach(mutation => {
-    let elementClassList = mutation.target.classList;
-    let isHeader = elementClassList.contains('header') && elementClassList.length === 1;
-    let isGist = elementClassList.contains('gist-header');
+    const elementClassList = mutation.target.classList
+    const isHeader = elementClassList.contains("header") && elementClassList.length === 1
+    const isGist = elementClassList.contains("gist-header")
     
     if (isHeader || isGist) {
-      mutation.target.classList.add('revive');
-      observer.disconnect();
+      mutation.target.classList.add("revive")
+      observer.disconnect()
     }
-  });
-});
+  })
+})
 
 observer.observe(document, {
   childList: true,
   subtree: true
-});
+})
